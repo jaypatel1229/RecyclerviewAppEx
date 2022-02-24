8aa91bf55b440ac949c94c15ba81462edfdb6e60
jay.patel <jay.patel@1R-80>
2022-02-24 09:59:14 +0530

    Add project files.

---
+++ RecyclerView_AppEx/EmployeeAdapter.cs
@@ -0,0 +1,79 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.RecyclerView.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RecyclerView_AppEx
+{
+
+    class EmployeeAdapter : RecyclerView.Adapter
+
+    {
+        private List<string> _employeeName;
+        public event EventHandler<int> ItemClick;
+
+        public EmployeeAdapter(List<string> employeeName)
+        {
+            _employeeName = employeeName;
+        }
+
+        public override int ItemCount => _employeeName.Count;
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View view = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.newemployee_listrow_item, parent, false);
+            //sends the inflated layout to the viewholder class that extends  : RecyclerView.ViewHolder
+            return new EmlpoyeeViewHolder(view, OnClick);
+
+        }
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+
+            EmlpoyeeViewHolder emlpoyeeViewHolder = holder as EmlpoyeeViewHolder;
+            emlpoyeeViewHolder.Binddata(_employeeName[position]);
+
+        }
+
+       
+
+        public void OnClick(int position)
+        {
+            if (ItemClick != null)
+            {
+                ItemClick?.Invoke(this, position);
+            }
+
+            ItemClick?.Invoke(this, position);
+        }
+
+
+        class EmlpoyeeViewHolder : RecyclerView.ViewHolder
+        {
+            public TextView _employeeNameText;
+
+
+            public EmlpoyeeViewHolder(View itemView, Action<int> listner) : base(itemView)
+            {
+                _employeeNameText = itemView.FindViewById<TextView>(Resource.Id._employeeText);
+                _employeeNameText.Click += (sender, e) => listner(base.LayoutPosition);
+            }
+
+
+            internal void Binddata(string employeeName)
+            {
+                _employeeNameText.Text = employeeName;
+            }
+
+
+        }
+    }
+
+
+}
---
+++ RecyclerView_AppEx/MainActivity.cs
@@ -0,0 +1,156 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using AndroidX.RecyclerView.Widget;
+using System;
+using System.Collections.Generic;
+
+namespace RecyclerView_AppEx
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        private RecyclerView _employeeRecyclerView;
+        
+        private List<string> _employeeName;
+        private EmployeeAdapter _employeeAdapter;
+        RecyclerView.LayoutManager _layoutManager;
+
+
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            _employeeRecyclerView = FindViewById<RecyclerView>(Resource.Id._albumRecyclerView);
+          
+
+
+            GenerateDummyRecord();
+
+
+
+            _layoutManager = new LinearLayoutManager(this);
+            _employeeRecyclerView.SetLayoutManager(_layoutManager);
+
+            _employeeAdapter = new EmployeeAdapter(_employeeName);
+            _employeeRecyclerView.SetAdapter(_employeeAdapter);
+            _employeeAdapter.ItemClick += _employeeAdapter_ItemClick;
+
+        }
+
+        private void _employeeAdapter_ItemClick(object sender, int e)
+        {
+            Toast.MakeText(this, "Hi " + _employeeName[e].ToString(), ToastLength.Short).Show();
+        }
+
+
+        private void GenerateDummyRecord()
+
+        {
+            _employeeName = new List<string>();
+
+
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Chandler");
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Chandler");
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Chandler");
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Chandler");
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Chandler");
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Chandler");
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Chandler");
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Chandler");
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Chandler");
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Chandler");
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Chandler");
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Chandler");
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Chandler");
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Ross");
+            _employeeName.Add("Rachel");
+            _employeeName.Add("Monica");
+            _employeeName.Add("Joey");
+            _employeeName.Add("Chandler");
+            _employeeName.Add("Chandler");
+            _employeeName.Add("Chandler");
+            _employeeName.Add("Chandler");
+
+
+
+
+
+        }
+
+
+
+    }
+}
